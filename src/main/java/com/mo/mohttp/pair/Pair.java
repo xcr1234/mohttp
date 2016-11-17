0402b6676d77604bfafe3321560edfe20de9fbd9
xcr <530551426@qq.com>
2016-11-17 16:36:27 +0800

    pair的小修复

--- src/main/java/com/mo/mohttp/pair/Pair.java
+++ src/main/java/com/mo/mohttp/pair/Pair.java
@@ -3,7 +3,7 @@ package com.mo.mohttp.pair;
 
 import java.io.Serializable;
 
-public abstract class Pair<K extends Serializable,V extends Serializable> implements Serializable{
+public class Pair<K extends Serializable,V extends Serializable> implements Serializable{
     private static final long serialVersionUID = 8140444398032758168L;
     private K name;
     private V value;
@@ -30,5 +30,29 @@ public abstract class Pair<K extends Serializable,V extends Serializable> implem
 
     public void setValue(V value) {
         this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+
+        if (name != null ? !name.equals(pair.name) : pair.name != null) return false;
+        return value != null ? value.equals(pair.value) : pair.value == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (value != null ? value.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(name)+"="+String.valueOf(value);
     }
 }
